254412e28f4bc69deaf6b6798f4cd61161214d91
Lann Martin <lann.martin@fermyon.com>
2022-12-09 16:22:18 -0500

    s3-upload: Make it actually work!
    Signed-off-by: Lann Martin <lann.martin@fermyon.com>

--- s3-upload/index.js
+++ s3-upload/index.js
@@ -1,52 +1,57 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-    const src = core.getInput('src');
-
-    const env = {
-        'AWS_REGION': core.getInput('region'),
-        'AWS_ACCESS_KEY_ID': core.getInput('access-key-id'),
-        'AWS_SECRET_ACCESS_KEY': core.getInput('secret-access-key'),
-    };
-    core.info(`Region: ${env['AWS_REGION']}`);
-
-    // Parse destination(s) from "<bucket>/<key>" to [<bucket>, <key>]
-    const dests = core.getMultilineInput('dest').map((dest) => {
-        const parts = dest.split('/');
-        if (parts.length < 2) {
-            throw new Error(`dest should be specified as '<bucket>/<key>'; got ${dest}`);
-        }
-        const bucket = parts.shift();
-        const key = parts.join('/');
-        return [bucket, key];
-    });
-
-    // Upload object
-    const [bucket, key] = dests[0];
-    core.info(`Uploading ${src} to ${bucket}/${key}`);
-    await exec.exec('aws', [
-        's3api', 'put-object',
-        '--bucket', bucket,
-        '--key', key,
-        '--body', src,
-        '--acl', 'public-read',
-    ], {env});
-
-    // Make any requested copies
-    const copy_src = `${bucket}/${key}`;
-    for (const [bucket, key] of dests.slice(1)) {
-            core.info(`Copying ${copy_src} to ${bucket}/${key}`);
-            await exec.exec('aws', [
-                's3api', 'copy-object',
-                '--copy-source', copy_src,
-                '--bucket', bucket,
-                '--key', key,
-                '--acl', 'public-read',
-            ], {env});
+function splitBucketAndKey(dest) {
+    const parts = dest.split('/');
+    if (parts.length < 2) {
+        throw new Error(`dest should be specified as '<bucket>/<key>'; got ${dest}`);
     }
-
-    core.setOutput("public-url", `https://${bucket}.s3.amazonaws.com/${key}`);
-} catch (err) {
-    core.setFailed(err.message);
+    const bucket = parts.shift();
+    const key = parts.join('/');
+    return [bucket, key];
 }
+
+async function run() {
+    try {
+        const src = core.getInput('src');
+
+        const env = {
+            'AWS_REGION': core.getInput('region'),
+            'AWS_ACCESS_KEY_ID': core.getInput('access-key-id'),
+            'AWS_SECRET_ACCESS_KEY': core.getInput('secret-access-key'),
+        };
+        core.info(`Region: ${env['AWS_REGION']}`);
+
+        // Parse copy dest(s) before upload
+        const copies = core.getMultilineInput('copies').map(splitBucketAndKey);
+
+        // Upload object
+        const [bucket, key] = splitBucketAndKey(core.getInput('dest'));
+        core.info(`Uploading ${src} to ${bucket}/${key}`);
+        await exec.exec('aws', [
+            's3api', 'put-object',
+            '--bucket', bucket,
+            '--key', key,
+            '--body', src,
+            '--acl', 'public-read',
+        ], {env});
+        core.setOutput("public-url", `https://${bucket}.s3.amazonaws.com/${key}`);
+
+        // Make copies
+        const copy_src = `${bucket}/${key}`;
+        for (const [bucket, key] of copies) {
+                core.info(`Copying ${copy_src} to ${bucket}/${key}`);
+                await exec.exec('aws', [
+                    's3api', 'copy-object',
+                    '--copy-source', copy_src,
+                    '--bucket', bucket,
+                    '--key', key,
+                    '--acl', 'public-read',
+                ], {env});
+        }
+    } catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run()
